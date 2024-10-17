4ec009cb80ecbd82a1e6ce675b818d3b670cfedd
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-10-18 04:43:28 +0500

    how to use img tag and component with relative and absolute url

--- next_js_learning/next.config.mjs
+++ next_js_learning/next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // image configiration
+    images: {
+        domains: ['img.freepik.com'],
+    },
 
+    // build configuration
+    // target: 'serverless',
     // generate static html pages
     // output: 'export'
 };
--- next_js_learning/src/app/image/page.tsx
+++ next_js_learning/src/app/image/page.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
-
+// import HeaderImg from './hello.jfif'
+import HeaderImg from './arsalan.png';
+import Image from 'next/image';
 export default function ImagePage() {
     return (
-        <div className='grid grid-cols-4'>
-            <img src='https://source.unsplash.com/featured/1920x1080?sig=1' alt='random pic' />
+        <div className=''>
+            <div>
+
+                <img src='https://tse3.mm.bing.net/th?id=OIP.j4KdqaXpnhbN94WzVyHUhAHaE8&pid=Api&P=0&h=220' alt='random pic' />
+            </div>
+            <div>
+                <img src="/lion.jfif" alt="" />
+
+            </div>
+            <div>
+                {/* <img src={HeaderImg} alt="" /> */}
+                <Image
+                    src={HeaderImg}
+                    width={200}
+                    height={300}
+                    alt='hello world'
+                />
+                <Image
+                    src='/main.png'
+                    width={200}
+                    height={300}
+                    alt='hello world'
+                />
+                <Image
+                    src='https://img.freepik.com/free-photo/abstract-autumn-beauty-multi-colored-leaf-vein-pattern-generated-by-ai_188544-9871.jpg?size=626&ext=jpg&ga=GA1.1.2113030492.1729123200&semt=ais_hybrid'
+                    width={200}
+                    height={300}
+                    alt='hello world'
+                />
+            </div>
+
 
         </div>
     )
