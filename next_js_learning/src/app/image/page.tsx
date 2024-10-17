1e6715e451cb4cda76ff55390908abbc5108d88d
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-10-18 04:26:48 +0500

    style and css modular

---
+++ next_js_learning/src/app/image/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function ImagePage() {
+    return (
+        <div className='grid grid-cols-4'>
+            <img src='https://source.unsplash.com/featured/1920x1080?sig=1' alt='random pic' />
+
+        </div>
+    )
+}
--- next_js_learning/src/app/page.tsx
+++ next_js_learning/src/app/page.tsx
@@ -1,19 +1,26 @@
 import Link from "next/link";
+import Heading from "./style.module.css";
 
 export default function Home() {
   return (
     <>
-      {/* <h1>hello world</h1> */}
-      <h1>Home Page</h1>
+      <h1>hello world</h1>
+      {/* modular css */}
+      <h1 className={Heading.h1}>Home Page</h1>
+      <h1 className={Heading.h1}>moduler css</h1>
 
 
       {/* linking */}
-      <div className="">
+      <div className="flex gap-2">
 
         <Link href='/about'>about</Link>
         <Link href='/contact'>contact</Link>
         <Link href='/testing'>testing</Link>
-
+        <p style={{
+          background: 'yellow',
+          padding: '10px',
+          textAlign: 'center'
+        }}>hello world</p>
       </div>
     </>
   )
